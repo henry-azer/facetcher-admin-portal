625951b91c3a74597a31b03a9ace3c50779524c0
unknown <a_bavly@yahoo.com>
2023-03-14 18:50:16 +0200

    using formik and yup package to login and get access token

--- src/constants/app_constants.jsx
+++ src/constants/app_constants.jsx
@@ -1,4 +1,7 @@
 export const APIs_URL = {
-	STAGING: "",
-	PRODUCTION: "http://127.0.0.1:8080"
+     STAGING:
+          "http://ec2-3-138-186-26.us-east-2.compute.amazonaws.com:8010/facetcher-service/api",
+     PRODUCTION: "http://127.0.0.1:8080",
 };
+export const ISUSERAUTH = "%mmNJ9B7K64$";
+export const ACCESSTOKEN = "%807&tI$4Wzm";
