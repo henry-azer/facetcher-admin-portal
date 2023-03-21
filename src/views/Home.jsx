483b37563ce69f6133b5ae66de2491fdf2e4cd46
unknown <a_bavly@yahoo.com>
2023-03-21 13:52:57 +0200

    handle chart in home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -11,6 +11,7 @@ import {
      ResponsiveContainer,
      Area,
      YAxis,
+     Legend,
 } from "recharts";
 import FacetcherDrawer from "../components/drawer/drawer";
 import { CYAN, LIGHTGREY, LIGHTGREY10T, ORANGE } from "../constants/app_colors";
@@ -21,8 +22,8 @@ const Home = () => {
      const dataView = () => {
           const data = [];
           for (let index = 0; index < 30; index++) {
-               const val = Math.random() * 11;
-               const val2 = Math.random() * 11;
+               const val = Math.ceil(Math.random() * 11);
+               const val2 = Math.ceil(Math.random() * 11);
                data.push({
                     day: index + 1,
                     value: val,
@@ -46,26 +47,30 @@ const Home = () => {
                          </p>
                     </div>
                     <div className="row justify-content-center align-items-center g-2">
-                         <div className="col-9 bg-dark-grey mx-2 p-3">
+                         <div className="col-8 bg-dark-grey mx-2 p-3">
                               <div>
                                    <ResponsiveContainer
                                         width="100%"
                                         height={300}
                                    >
-                                        <AreaChart data={dataView()}>
-                                             <Area
+                                        <LineChart data={dataView()}>
+                                             <Line
+                                                  type="monotone"
                                                   dataKey="value"
                                                   stroke={CYAN}
                                                   strokeWidth="2"
                                                   fill="transparent"
                                              />
-                                             <Area
+                                             <Line
+                                                  type="monotone"
                                                   dataKey="value2"
                                                   stroke={ORANGE}
                                                   strokeWidth="2"
                                                   fill="transparent"
                                              />
-                                             <Area
+                                             <Line
+                                                  activeDot={{ r: 8 }}
+                                                  type="monotone"
                                                   dataKey="value3"
                                                   stroke={LIGHTGREY}
                                                   strokeWidth="2"
@@ -85,8 +90,10 @@ const Home = () => {
                                              <CartesianGrid
                                                   stroke={LIGHTGREY10T}
                                                   vertical={false}
+                                                  strokeDasharray="5 5"
                                              />
-                                        </AreaChart>
+                                             <Legend/>
+                                        </LineChart>
                                    </ResponsiveContainer>
                               </div>
                          </div>
