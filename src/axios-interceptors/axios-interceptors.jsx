7478633acf7409472f6c121e20c079c3f550a5da
Henry-Azer <henryazer@outlook.com>
2023-05-14 02:06:30 +0300

    refactor ACCESS_TOKEN and IS_USER_AUTHENTICATED values

--- src/axios-interceptors/axios-interceptors.jsx
+++ src/axios-interceptors/axios-interceptors.jsx
@@ -1,52 +1,22 @@
 import axios from "axios";
-import { ACCESSTOKEN } from "../constants/app_constants";
 import Cookies from "universal-cookie";
-import { navigateToLogin } from "../utils/util";
+import { ACCESS_TOKEN } from "../constants/app_constants";
 
 const cookies = new Cookies();
 
 axios.interceptors.request.use((config) => {
-     const token = cookies.get(ACCESSTOKEN);
-     if (token && token !== "undefined")
-          config.headers.Authorization = `Bearer ${token}`;
-     return config;
+    const token = cookies.get(ACCESS_TOKEN); 
+    if (token && token !== "undefined") {
+         config.headers.Authorization = `Bearer ${token}`;
+     }
+    return config;
 });
 
 axios.interceptors.response.use(
-     (response) => {
-          // handle success
-          return response;
-     },
-     (error) => {
-          // handle error
-          if (error.response.status === 401) {
-               // redirect to login page
-               navigateToLogin();
-          }
-          return Promise.reject(error);
-     }
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
 );
-
-// instance.interceptors.request.use(
-//      function(config) {
-//           // do something before request is sent
-//           return config;
-//      },
-//      function(error) {
-//           // do something with request error
-//           return Promise.reject(error);
-//      }
-// );
-
-// instance.interceptors.response.use(
-//      function(response) {
-//           // do something with response data
-//           return response;
-//      },
-//      function(error) {
-//           // do something with response error
-//           return Promise.reject(error);
-//      }
-// );
-
-// export default instance;
--- src/constants/app_constants.jsx
+++ src/constants/app_constants.jsx
@@ -5,8 +5,8 @@ export const APIs_URL = {
           "http://ec2-18-216-155-91.us-east-2.compute.amazonaws.com:8010/facetcher-service/api",
      PRODUCTION: "http://127.0.0.1:8080",
 };
-export const ISUSERAUTH = "%mmNJ9B7K64$";
-export const ACCESSTOKEN = "%807&tI$4Wzm";
+export const ACCESS_TOKEN = "%807&tI$4Wzm";
+export const IS_USER_AUTHENTICATED = "%mmNJ9B7K64$";
 
 export const DASHBOARD = "Dashboard";
 export const ALL_USERS = "All Users";
