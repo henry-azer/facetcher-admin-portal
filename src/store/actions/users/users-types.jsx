61326a50a6a3bf9e1adff3f8d4ca365a2c45f2d3
Henry-Azer <henryazer@outlook.com>
2023-05-13 15:08:03 +0300

    add user profile picture request types values

--- src/store/actions/users/users-types.jsx
+++ src/store/actions/users/users-types.jsx
@@ -4,3 +4,7 @@ export const CLEAR_REGISTRATION_DETAILS = "clear_registration_details";
 export const USER_ADDED_SUCCESSFULLY = "user_added_successfully";
 export const ADDING_USER = "adding_user";
 export const ADDING_USER_FAILED = "adding_user_failed";
+
+export const USER_PROFILE_PICTURE_REQUEST = "user_profile_picture_request";
+export const USER_PROFILE_PICTURE_SUCCEEDED = "user_profile_picture_succeeded";
+export const USER_PROFILE_PICTURE_ERROR = "user_profile_picture_error";
